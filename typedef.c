aa0900db29486960b8fb6956d3c7c22d65152d3e
azarMohd <azar.mohammed4u@gmail.com>
2023-09-19 12:24:11 +0530

    first updates

---
+++ typedef.c
@@ -0,0 +1,32 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+typedef int i1;
+typedef long long l1;
+int main() {
+//     Write C code here
+    i1 a=20;
+    l1 b=1234567890123456789;
+    printf("Hello world %d\n",a);
+    printf("Hello world2 %lld",b);
+     char *s="c++";
+    int *a=10;
+    printf("%s\n",s);
+        printf("%d\n",a++);
+        printf("%d",a);
+
+
+
+int min=__SHRT_MAX__;
+int max=__SHRT_MAX__;
+printf("min value of the compiler:%d\n",min);
+printf("maximum value of the compiler:%d",max);
+
+
+
+
+
+
+
+
+    return 0;
+}
